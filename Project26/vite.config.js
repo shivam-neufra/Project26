012e5c6077936c30fc6032133404c7b26cc67447
KetanChavan24 <ketanchavan248@gmail.com>
2024-07-30 18:55:13 +0530

    edited for deploying on github pages

--- Project26/vite.config.js
+++ Project26/vite.config.js
@@ -1,10 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
-});
+  base: 'Project26',  // Replace <REPO_NAME> with your repository name
+})
